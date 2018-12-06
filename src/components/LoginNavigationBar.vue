f12aa38333f94000ee8fa557c43666fdb8d883d2
Max-Loo <1124843223@qq.com>
2018-12-06 22:14:02 +0800

    添加登陆页面顶部导航栏

---
+++ src/components/LoginNavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="LoginNavigation">
+      <div class="Logo">LOGO</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginNavigation'
+}
+</script>
+
+<style scoped>
+.LoginNavigation{
+  width: 100%;
+  height: 50px;
+  background-color: #333;
+  margin: 0px;
+  display: flex;
+  align-items: center;
+}
+.Logo{
+  color: white;
+  font-size: 30px;
+  position: absolute;
+  left: 7%;
+}
+</style>
