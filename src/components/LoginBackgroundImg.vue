978c814d9c1f4b3e1b448198a5646f5700da94af
Max-Loo <1124843223@qq.com>
2018-12-06 22:13:09 +0800

    添加登陆页面背景图片

---
+++ src/components/LoginBackgroundImg.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="../assets/back.jpg" class="LoginBackgroundImg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginBackgroundImg'
+}
+</script>
+
+<style scoped>
+.LoginBackgroundImg{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -1000;
+}
+</style>
