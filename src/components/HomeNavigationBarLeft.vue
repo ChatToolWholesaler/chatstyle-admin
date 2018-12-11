f23812bf5528e23acff06a7eeec28f3e22e9e269
Max-Loo <1124843223@qq.com>
2018-12-11 17:20:48 +0800

    修改主页面按钮的样式

--- src/components/HomeNavigationBarLeft.vue
+++ src/components/HomeNavigationBarLeft.vue
@@ -1,20 +1,68 @@
 <template>
-  <div class="HomeNavigationBarLeft">
-    <ul></ul>
+  <div class="homeNavigationBarLeft">
+    <div class="userBox">
+      <img id="Avatar" class="avatar"/>
+      <div class="name">{{ username }}</div>
+      <div class="userTag">{{ userTag }}</div>
+    </div>
+    <div class="buttonList">
+      <home-navigation-bar-left-button button-name="用户列表"/>
+      <home-navigation-bar-left-button button-name="公告列表"/>
+      <home-navigation-bar-left-button button-name="房间列表"/>
+    </div>
   </div>
 </template>
 
 <script>
+import HomeNavigationBarLeftButton from '@/components/HomeNavigationBarLeftButton'
 export default {
-  name: 'HomeNavigationBarLeft'
+  name: 'HomeNavigationBarLeft',
+  components: {
+    HomeNavigationBarLeftButton
+  },
+  computed: {
+    username () {
+      return '吴彦祖'
+    },
+    userTag () {
+      return '管理员'
+    }
+  }
 }
 </script>
 
 <style scoped>
-.HomeNavigationBarLeft{
+.homeNavigationBarLeft{
   width: 200px;
   height: 100%;
   background-color: #333;
   position: fixed;
+}
+.userBox{
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+.avatar{
+  background-color: white;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+.name{
+  color:white;
+  width: 100%;
+  height: fit-content;
+  font-size: 30px;
+}
+.userTag{
+  color:white;
+  width: 100%;
+  height: fit-content;
+  font-size: 15px;
+  margin-top: 5px;
+}
+.buttonList{
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 </style>
---
+++ src/components/HomeNavigationBarLeftButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="buttonBox">
+    <!-- <div :class="{ a: isActive,  b: !isActive }"></div> -->
+  <div :class="[isActive ? buttonActive : '', button]">
+      <div>{{ buttonName }}</div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeNavigationBarLeftButton',
+  props: ['buttonName', 'button']
+}
+</script>
+
+<style scoped>
+.buttonBox{
+  color: white;
+  width: 100%;
+  height: 50px;
+}
+.buttonBox:hover{
+  background-color: #949494;
+}
+.buttonBox:active{
+  background-color: #0063b1;
+}
+.button{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
