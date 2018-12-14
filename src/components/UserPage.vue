5fbfc3b765cac803bd7085a7d2b8dfbd50af13fd
Max-Loo <1124843223@qq.com>
2018-12-14 17:28:56 +0800

    '尝试添加用户信息页面'

---
+++ src/components/UserPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="userPage">
+    <div class="userStatistic">用户信息</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage'
+}
+</script>
+
+<style scoped>
+.userPage{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 1200px;
+}
+.userStatistic{
+  width: 1000px;
+  height: 300px;
+  background-color: white;
+}
+</style>
