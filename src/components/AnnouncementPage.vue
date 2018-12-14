63f38f64164d81cc1d47466b9f2a1cef8396862c
Max-Loo <1124843223@qq.com>
2018-12-14 17:29:24 +0800

    '尝试添加公共页面'

---
+++ src/components/AnnouncementPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="announcementPage">
+    <div class="announcementStatistic">公告信息</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnnouncementPage'
+}
+</script>
+
+<style scoped>
+.announcementPage{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 1200px;
+}
+.announcementStatistic{
+  width: 1000px;
+  height: 300px;
+  background-color: white;
+}
+</style>
