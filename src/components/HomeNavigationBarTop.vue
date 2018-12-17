415aa97fccfc0665aa5bac7d25808eef74c9316b
Max-Loo <1124843223@qq.com>
2018-12-17 12:24:10 +0800

    '添加注销按钮'

--- src/components/HomeNavigationBarTop.vue
+++ src/components/HomeNavigationBarTop.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="HomeNavigationBarTop">
       <div class="Logo">LOGO</div>
+      <div class="logout"></div>
     </div>
   </div>
 </template>
@@ -21,11 +22,22 @@ export default {
   display: flex;
   align-items: center;
 }
-
 .Logo{
   color: white;
   font-size: 30px;
   position: absolute;
   left: 55px;
+}
+.logout{
+  width: 30px;
+  height: 30px;
+  background-image: url(../assets/power.png);
+  background-size: contain;
+  position: absolute;
+  right: 50px;
+  cursor: pointer;
+}
+.logout:hover{
+  background-image: url(../assets/powerHover.png);
 }
 </style>
